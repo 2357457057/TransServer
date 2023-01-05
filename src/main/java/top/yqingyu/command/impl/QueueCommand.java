1c6904f2fa9ffd73480408954aa43dca133277a9
SeverusQy <yangyongji123@live.con>
2023-01-06 03:53:24 +0800

    升级QyMsg

--- src/main/java/top/yqingyu/command/impl/QueueCommand.java
+++ src/main/java/top/yqingyu/command/impl/QueueCommand.java
@@ -1,23 +1,15 @@
 package top.yqingyu.command.impl;
 
-import cn.hutool.core.date.LocalDateTimeUtil;
 import lombok.extern.slf4j.Slf4j;
-import top.yqingyu.bean.ClientInfo;
 import top.yqingyu.command.Command;
-import top.yqingyu.common.qydata.DataMap;
-import top.yqingyu.common.qymsg.MsgHelper;
 import top.yqingyu.common.qymsg.MsgTransfer;
 import top.yqingyu.common.qymsg.QyMsg;
-import top.yqingyu.component.RegistryCenter;
 import top.yqingyu.main.MainConfig;
 
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.SocketChannel;
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.FutureTask;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
+
 
 /**
  * @author YYJ
@@ -33,13 +25,8 @@ public class QueueCommand implements Command {
     @Override
     public void commandDeal(SocketChannel socketChannel, Selector selector, QyMsg msg) throws Exception {
         socketChannel.register(selector, SelectionKey.OP_WRITE);
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < 1024; i++) {
-            sb.append("12345678901234567890");
-        }
         QyMsg clone = MainConfig.NORM_MSG.clone();
-        clone.putMsg(sb.toString());
+        clone.putMsg("12345678901234567890".repeat(10240));
         MsgTransfer.writeQyMsg(socketChannel, clone);
         socketChannel.register(selector, SelectionKey.OP_READ);
     }
