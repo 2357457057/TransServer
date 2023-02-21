b8accc450e717366b83a3563fc1ebae6cf935e97
Severus <yangyongji123@live.com>
2023-02-21 19:51:41 +0800

    filesystem -cd  命令修复

---
+++ src/main/java/top/yqingyu/trans$server/command/impl/Download.java
@@ -0,0 +1,20 @@
+package top.yqingyu.trans$server.command.impl;
+
+import top.yqingyu.common.qymsg.QyMsg;
+import top.yqingyu.trans$server.command.ParentCommand;
+
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+import java.util.ArrayList;
+
+public class Download extends ParentCommand {
+    public Download() {
+        super("download");
+    }
+
+    @Override
+    protected void deal(SocketChannel socketChannel, Selector selector, QyMsg msg, ArrayList<QyMsg> rtnMsg) throws Exception {
+
+    }
+}
+
